678ea270324c335375456f34d86a16da8e761b78
Bolivar <bolhy91@gmail.com>
2023-11-27 13:43:49 -0500

    Initial Commit

---
+++ SwiftDataTutorial/SwiftDataTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTutorialApp.swift
+//  SwiftDataTutorial
+//
+//  Created by Bolivar Cortes on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
